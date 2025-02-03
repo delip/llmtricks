ebad2399df71f27ceb837d46e2ab5994dbc45444
Delip Rao <deliprao@gmail.com>
2025-02-02 22:30:03 -0500

    updates

--- build.sh
+++ build.sh
@@ -7,5 +7,10 @@ if [ $? -ne 0 ]; then
     echo "update_version.py failed"
     exit 1
 fi
+VERSION=`cat pyproject.toml | grep version | cut -d'=' -f2 | tr -d ' '`
+echo "Building version $VERSION"
 python -m build
 twine upload dist/*
+git add .
+git commit -m "Bump version to $VERSION"
+git push
